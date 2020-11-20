06f06b03b18075a34a24c7ec3980d3271b3f30c6
Francis Champagne <hey@fcis.io>
2020-11-20 12:41:53 -0500

    create split theme fixture

---
+++ tests/fixtures/split-theme/scales.js
@@ -0,0 +1,23 @@
+// export default {
+//   space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+//   borders: {
+//     body: "2px solid",
+//   },
+//   sizes: {
+//     sidebar: 256,
+//     colorswitch: "1.15em",
+//     container: 1000,
+//   },
+// };
+
+export const scales = {
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+  borders: {
+    body: "2px solid",
+  },
+  sizes: {
+    sidebar: 256,
+    colorswitch: "1.15em",
+    container: 1000,
+  },
+};
